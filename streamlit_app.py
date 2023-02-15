3392fa9602d8db5785536f69f585380a556ee534
gb25564 <125471842+gb25564@users.noreply.github.com>
2023-02-15 18:03:26 -0500

    Update streamlit_app.py
    format the json response to a table for display

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize the json from the response json 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# loads the normalized data into a dataframe
+streamlit.dataframe(fruityvice_normalized)
