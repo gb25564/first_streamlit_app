79f0108d402bf5f5dbe8612f2485c8836350445f
gb25564 <125471842+gb25564@users.noreply.github.com>
2023-02-16 09:45:21 -0500

    Update streamlit_app.py
    changed labels

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ try:
 except URLError as e:
     streamlit.error()
 
-streamlit.header("The Fruit Load List Contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!:")
 
 #Snowflake related function
 def get_fruit_load_list():
@@ -53,7 +53,7 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 # add a button to load the fruit    
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
