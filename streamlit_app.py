f4af34ae0ff4a0d8ce3dea664ad16ee1161156b3
gb25564 <125471842+gb25564@users.noreply.github.com>
2023-02-16 08:23:58 -0500

    Update streamlit_app.py
    added control flow and added additional libraries for inserts into fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -37,6 +38,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # loads the normalized data into a dataframe
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -46,3 +49,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
