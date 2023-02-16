70afbebe7978ecbe4732fc9d39fc7d832898e651
gb25564 <125471842+gb25564@users.noreply.github.com>
2023-02-16 08:34:59 -0500

    Update streamlit_app.py
    added try else error

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,15 +28,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    #streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response.json())
 
-# normalize the json from the response json 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# loads the normalized data into a dataframe
-streamlit.dataframe(fruityvice_normalized)
+    # normalize the json from the response json 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # loads the normalized data into a dataframe
+    streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+    streamlit.error()
 
 streamlit.stop()
 
