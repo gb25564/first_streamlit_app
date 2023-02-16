b7ee7b73393918d9dd13d68b848d5b9710e58231
gb25564 <125471842+gb25564@users.noreply.github.com>
2023-02-16 08:47:39 -0500

    Update streamlit_app.py
    added function to get fruit information from API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,21 +27,20 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
-    #streamlit.write('The user entered ', fruit_choice)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    #streamlit.text(fruityvice_response.json())
-
-    # normalize the json from the response json 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     # loads the normalized data into a dataframe
-    streamlit.dataframe(fruityvice_normalized)
-
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
 
